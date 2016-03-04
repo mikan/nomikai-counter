8e7625936fd4d77c6f3cacad51c9d46c1fc8c48b
ykatoh <yutaka.yk.katoh@nts.ricoh.co.jp>
2016-03-04 10:21:39 +0900

    Added servlet initializer.

---
+++ src/main/kotlin/com/github/mikan/nomikaicounter/ServletInitializer.kt
@@ -0,0 +1,16 @@
+package com.github.mikan.nomikaicounter
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+import org.springframework.context.annotation.Configuration
+
+/**
+ * @author mikan
+ * @since 2.0
+ */
+@Configuration
+open class ServletInitializer : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(App::class)
+    }
+}
